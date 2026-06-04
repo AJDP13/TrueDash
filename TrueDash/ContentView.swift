760887e3e00cbd7ca8ad14619a7ce2cf039cb9b3
AJ Tech City <arunjai12456@icloud.com>
2026-06-04 22:40:45 +0100

    Initial Commit

---
+++ TrueDash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrueDash
+//
+//  Created by Arun Dutta-Plummer on 04/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrueDash/TrueDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueDashApp.swift
+//  TrueDash
+//
+//  Created by Arun Dutta-Plummer on 04/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct TrueDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
